a151af386cd308c48a6a5c9ba76d2fbc56048bec
Eric Espinoza <ericespinoza@outlook.com>
2015-10-26 18:14:58 -0700

    Created PlayerStats and its dependant classes. Started working on PlayerStats.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	public float secondsPerYear = 15f;
+
+	private int age;
+	private float money;
+	private float cashFlow;
+	private float happiness;
+	private float happinessDecreateRate;
+
+	// a year is 15 seconds
+	// cashFlow is per year
+	// happinessDecreaseRate is applied at the end of the year
+	// happiness is between 0 - 1
+
+	void Start () {
+		age = 16;
+		money = 1000f;
+		cashFlow = -200f;
+		happiness = 0.5f;
+		happinessDecreateRate = 0.1f;
+	}
+
+	void Update () {
+		
+	}
+}
