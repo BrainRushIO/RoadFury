78f39d4d5bc6c465a72919ee899cd3146df0326e
paleonluna <paleonluna@gmail.com>
2015-10-26 18:15:55 -0700

    mergin

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
-	float strafeSpeed = .1f, moveSpeed = .1f;
+	float strafeSpeed = .1f, moveSpeed = .2f;
 	float playerBounds = 4f;
 	public GameObject currentRoadSection;
 	bool isOnHorizontalRoad = false;
