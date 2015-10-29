23467b476dd67f3b3748d36f29b2d16764c611a4
Eric Espinoza <ericespinoza@outlook.com>
2015-10-29 15:12:29 -0700

    Number conversion class.

---
+++ Assets/Scripts/NumberToString.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberToString : MonoBehaviour {
+
+	public static string Convert( float number ) {
+		if( number >= 1000000000f ) {
+			number /= 1000000000f;
+			return System.Math.Round( number, 1 ).ToString() + "B"; 
+		} else if( number >= 1000000f ) {
+			number /= 1000000f;
+			return System.Math.Round( number, 1 ).ToString() + "M";
+		} else if( number >= 1000f ) {
+			number /= 1000f;
+			return System.Math.Round( number, 1 ).ToString() + "K";
+		} else {
+			return System.Math.Round( number, 2 ).ToString();
+		}
+	}
+}
