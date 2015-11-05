8d07ed065b1a6b4cf43a855dabf55a7a0b182f55
paleonluna <paleonluna@gmail.com>
2015-11-05 11:25:30 -0800

    fixed Pause Button, GUI menu pitstop, and more

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -160,7 +160,6 @@ public class PlayerController : MonoBehaviour {
 			pitstopEntranceAvailable = false;
 			respawnPos = other.GetComponent<PitStopRespawn> ().respawnPosition;
 		} else if (other.tag == "pitstopRoad") {
-			SoundtrackManager.s_instance.PlayAudioSource(SoundtrackManager.s_instance.pitstop);
 			pitstopEntranceAvailable = true;
 		} else if (other.tag == "tutorial") {
 			SoundtrackManager.s_instance.PlayAudioSource(SoundtrackManager.s_instance.click2);
@@ -182,6 +181,8 @@ public class PlayerController : MonoBehaviour {
 			}
 		} else if( other.tag == "pitstopRoad" ) {
 			pitstopEntranceAvailable = false;
+			SoundtrackManager.s_instance.PlayAudioSource(SoundtrackManager.s_instance.pitstop);
+
 		}
 	}
 
