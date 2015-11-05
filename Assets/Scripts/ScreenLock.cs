5ef6f557b9b17ce9e62a1e2e6942099a8741f818
paleonluna <paleonluna@gmail.com>
2015-11-05 13:31:35 -0800

    screen lock: '

---
+++ Assets/Scripts/ScreenLock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenLock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.orientation = ScreenOrientation.Portrait;
+		Screen.autorotateToLandscapeLeft = false;
+		Screen.autorotateToLandscapeRight = false;
+		Screen.autorotateToPortraitUpsideDown = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
